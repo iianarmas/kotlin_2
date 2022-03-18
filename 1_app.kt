ca569063453bcaa2c33a5aa79dd94fe2d7d20954
iian armas <armas.cav@gmail.com>
2022-03-18 19:38:50 +0800

    Create 1_app.kt

---
+++ 1_app.kt
@@ -0,0 +1,12 @@
+fun main (args: Array<String>){
+  // fun main(){}
+  var x = 1
+  
+  println("Before the loop. x = $x.)
+  
+  while (x < 5) {
+  println("In the loop. x = $x.")
+  x += 1
+  }
+  println("After the loop. x = $x.)
+}
