59d3ef21bbba9dfc098f8c46892b85bb37370361
iian armas <armas.cav@gmail.com>
2022-03-23 19:31:47 +0800

    Update 5_forLoop.kt

--- 5_forLoop.kt
+++ 5_forLoop.kt
@@ -15,3 +15,22 @@ for (i in 15 downTo 10) println(i)
 // use "step" to skip numbers in range
 
 for (i in 1..100 step 2) println(i)
+
+
+// looping through items in an array
+
+fun main(args: Array<String>) {
+    val options: Array<String> = arrayOf("Rock", "Paper", "Scissors")
+    
+    for (item in options) println("$item is an item in the array.")
+
+}
+
+// looping through items in an array with indeces
+
+fun main(args: Array<String>) {
+    val options: Array<String> = arrayOf("Rock", "Paper", "Scissors")
+
+    for ((ind, i) in options.withIndex()) println("Index $ind has item $i")
+
+}
