430ecea5e465e0be14ce7888b819941857c8b411
iian armas <armas.cav@gmail.com>
2022-03-23 20:07:32 +0800

    Create 6_operators.kt

---
+++ 6_operators.kt
@@ -0,0 +1,26 @@
+// || is OR
+
+if (price <= 10 || price >= 1000) {
+  // Your code here
+}
+
+
+// && is AND
+
+if (price >= 100 && price <= 200) {
+  // Your code here
+}
+
+
+// != is NOT EQUAL
+
+if (price!= 100) {
+  // Your code here
+}
+
+
+// ! is NOT
+
+while (!isBroken) {
+  // Do this if it is not broken
+}
