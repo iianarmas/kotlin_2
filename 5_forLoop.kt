003c0a742e2b1ca496850670e92d8e230bcc87c3
iian armas <armas.cav@gmail.com>
2022-03-23 19:21:27 +0800

    Update 5_forLoop.kt

--- 5_forLoop.kt
+++ 5_forLoop.kt
@@ -6,3 +6,12 @@ for (i in 1..100) println(i)
 // to print range that doesn't include the last number, use "until"
 
 for (i in 1 until 100) println(i)
+
+
+// use "downTo" to reverse the range
+
+for (i in 15 downTo 10) println(i)
+
+// use "step" to skip numbers in range
+
+for (i in 1..100 step 2) println(i)
